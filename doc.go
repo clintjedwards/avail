3d4cbd840e26c968c567401b3692a289726a245a
Clint J Edwards <clint.j.edwards@gmail.com>
2020-10-08 04:06:48 -0400

    add package doc file and update readme

--- doc.go
+++ doc.go
@@ -1,12 +1,11 @@
-# Avail(able)
-
-This package allows the representation of a time frame using common cron syntax and
+/*
+Package avail allows the representation of a time frame using common cron syntax and
 then efficiently check whether a given golang time object exists within that time frame.
 
 The package uses a subset of the extended cron standard:
 https://en.wikipedia.org/wiki/Cron#CRON_expression
 
-## Why
+Why
 
 It is sometimes useful to represent certain timeframes using cron expressions.
 
@@ -19,7 +18,7 @@ Using cron to achieve this allows the representation of situations like above to
 to parse. Other advantages include that the cron format is well documented and can potentially
 be represented in a user-friendly frontend component.
 
-## How
+How
 
 Avail implements/uses a stripped down version of the cron expression syntax as defined below.
 
@@ -38,7 +37,7 @@ and generates map backed sets for each field in order to allow speedy checking o
 This data structure allows avail to take a supplied time and check that each of the time's
 elements exist in the representation of the cron expression.
 
-## Usage
+Usage
 
 Initiate a new avail instance with cron expression. This will parse the cron expression given and
 return a new `Avail` object containing your given expression and its parsed terms(each section
@@ -52,3 +51,6 @@ Then call `able` with a specified go time object.
 
     fmt.Println(avail.Able(now))
     // Output: true
+
+*/
+package avail
